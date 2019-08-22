6c298343fc14ab23cd397e64f051d67e3bae429b
Epitácio <bessa.epitacio>
2019-08-22 01:25:30 -0300

    Melhorando código das threads e adicionando unroll com processos, porém não finalizados

--- primeiro-trabalho/unroll.py
+++ primeiro-trabalho/unroll.py
@@ -11,7 +11,7 @@ def matriz_randomica(rows, cols):
             matriz[i][j] = random.randint(0,10)
     return matriz
 
-def soma_matrizes(rowA, rowB, results):
+def soma_matrizes_threads(rowA, rowB, results):
     threading.currentThread()
     
     linha_somada = []
@@ -19,6 +19,15 @@ def soma_matrizes(rowA, rowB, results):
         linha_somada.append(b+a) # soma das matrizes
 
     results.append(linha_somada)
+
+def soma_matrizes_processos(rowA, rowB, processo, results):
+    processo = os.fork()
+    if processo == 0:
+        linha_somada = []
+        for a,b in zip(rowA, rowB):
+            linha_somada.append(b+a) # soma das matrizes
+
+        results.append(linha_somada)  
 
 def print_matriz(matriz):
     """Imprime matriz na tela, desde que ela tenha o formato [[]]"""
@@ -30,6 +39,8 @@ def print_matriz(matriz):
 def unroll(args, func, method, results):
     matriz_aleatoria = matriz_randomica(len(args), len(args[0]))
 
+    # ---------- Threads ----------
+    # Acredito que esteja finalizada, nao sei se tem algo errado ou faltando
     if method == "thre":
         threds = []
         for arg, row_aleatoria in zip(args, matriz_aleatoria):
@@ -38,21 +49,36 @@ def unroll(args, func, method, results):
             threds[-1].start()
 
         print("------ Args ------")
-        for i in args:
-            for arg in i:
-                print(str(arg), end=", ")
-            print()
-        
+        print_matriz(args)
+
         print("\n------ Aleatoria ------")
-        for ma in matriz_aleatoria:
-            for m in ma:
-                print(str(m), end=", ")
-            print()
-        
+        print_matriz(matriz_aleatoria)
+
+        print("\n------ Matriz soma ------")
+        print_matriz(results)
+    
+    # ---------- PROCESSOS ----------
+    # Ainda não esta pronto, eh preciso fazer com os processos se comuniquem
+    # provavelmente com memoria compartilhada so assim pra conseguir salvar os results 
+    # de cada soma das linhas da matriz.
+    # No caso o processo original devera imprimir a soma completa da matriz
+    else: 
+        processos = []
+
+        for arg, row_aleatoria in zip(args, matriz_aleatoria):
+            processos.append([])
+            func(arg, row_aleatoria, processos[-1], results)            
+
+        print("------ Args ------")
+        print_matriz(args)
+
+        print("\n------ Aleatoria ------")
+        print_matriz(matriz_aleatoria)
+
         print("\n------ Matriz soma ------")
         print_matriz(results)
 
 if __name__ == '__main__':
     res = []
-    # unroll([[0, 1,3],[2,3,4],[4,5,7]], func, 'proc', res)
-    unroll([[0, 1, 3, 4, 5],[2, 3, 1, 2, 3],[4, 5, 4, 2, 5]], soma_matrizes, 'thre', res)
+    unroll([[0, 1,3],[2,3,4],[4,5,7]], soma_matrizes_processos, 'proc', res)
+    # unroll([[0, 1, 3, 4, 5],[2, 3, 1, 2, 3],[4, 5, 4, 2, 5]], soma_matrizes_threads, 'thre', res)
