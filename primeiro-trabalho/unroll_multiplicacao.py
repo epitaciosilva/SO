03a2b602e0bb2ffd11d3e003f48d860eced66a2c
Italo Epifanio <italoepifaniols@gmail.com>
2019-09-07 16:28:58 -0300

    Adiciona funções uteis para a multiplicação de matrizes. Falta processos esperar uns pelos outros e se comunicarem

--- primeiro-trabalho/unroll_multiplicacao.py
+++ primeiro-trabalho/unroll_multiplicacao.py
@@ -1,6 +1,7 @@
 import os
 import random
 import threading
+from multiprocessing import Queue
 
 def matriz_randomica(rows, cols):
     matriz = []
@@ -12,21 +13,20 @@ def matriz_randomica(rows, cols):
             matriz[i][j] = random.randint(0,10)
     return matriz
 
-def multiplicacao_matrizes_processos(rowA, rowB, processo, results):
+def multiplicacao_matrizes_processos(row_a, col_a, processo, results):
     processo = os.fork()
-    if processo == 0:
-        linha_somada = []
-        for a,b in zip(rowA, rowB):
-            linha_somada.append(b+a) # soma das matrizes
+    if processo == 0: # se o processo for filho
+        results.append([a*b for a,b in zip(row_a, col_a)])
 
-        results.append(linha_somada)  
+def get_col(arr, col):
+    return list(map(lambda x : x[col], arr))
 
-def multiplicacao_matrizes_threads(row_A, col_B, index_row, index_col, results):
+def multiplicacao_matrizes_threads(row_a, col_b, index_row, index_col, results):
     threading.currentThread()
 
     soma = 0
-    for i in range(len(row_A)):
-        soma += row_A[i] * col_B[i]
+    for i in range(len(row_a)):
+        soma += row_a[i] * col_b[i]
     
     results[index_row][index_col] = soma
 
@@ -39,8 +39,8 @@ def print_matriz(matriz):
         print(" |")
 
 def unroll(args, func, method, results):
-    matriz_aleatoria = matriz_randomica(len(args[0]), random.randint(1,3))
-
+    # matriz_aleatoria = matriz_randomica(len(args[0]), random.randint(1,3))
+    matriz_aleatoria = [[1,1,1],[1,1,1],[1,1,1]]
     # ---------- Threads ----------
     # A soma de cada elemento é feito dentro de uma thread
     if method == "thre":
@@ -76,12 +76,21 @@ def unroll(args, func, method, results):
     # provavelmente com memoria compartilhada so assim pra conseguir salvar os results 
     # de cada soma das linhas da matriz.
     # No caso o processo original devera imprimir a soma completa da matriz
-    else: 
+    else:
         processos = []
-
-        for arg, row_aleatoria in zip(args, matriz_aleatoria):
-            processos.append([])
-            func(arg, row_aleatoria, processos[-1], results)            
+        # Dimensão das matrizes
+        cols = len(matriz_aleatoria[0])
+        # rows = len(matriz_aleatoria)
+        # arg = []
+        for arg in args:
+            for col in range(cols):
+                processos.append([])
+                func(arg, get_col(matriz_aleatoria,col), processos[-1], results)
+                break
+        
+        # for arg, row_aleatoria in zip(args, matriz_aleatoria):
+        #     processos.append([])
+        #     func(arg, row_aleatoria, processos[-1], results)            
 
         print("------ Args ------")
         print_matriz(args)
@@ -89,10 +98,11 @@ def unroll(args, func, method, results):
         print("\n------ Aleatoria ------")
         print_matriz(matriz_aleatoria)
 
-        print("\n------ Matriz soma ------")
+        print("\n------ Matriz multiplicada ------")
         print_matriz(results)
 
 if __name__ == '__main__':
     res = []
-    # unroll([[0, 1,3],[2,3,4],[4,5,7]], multiplicacao_matrizes_processos, 'proc', res)
-    unroll([[0, 1, 3],[2, 3, 1],[4, 5, 4]], multiplicacao_matrizes_threads, 'thre', res)
+    # results = Queue()
+    unroll([[0, 1, 3],[2, 3, 1],[4, 5, 4]], multiplicacao_matrizes_processos, 'proc', res)
+    # unroll([[0, 1, 3],[2, 3, 1],[4, 5, 4]], multiplicacao_matrizes_threads, 'thre', res)
