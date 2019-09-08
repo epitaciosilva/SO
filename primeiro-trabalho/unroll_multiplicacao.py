8b3828f07fe8c9992d6c269569c93a6a6f8a8529
Italo Epifanio <italoepifaniols@gmail.com>
2019-09-08 17:11:35 -0300

    Dale

--- primeiro-trabalho/unroll_multiplicacao.py
+++ primeiro-trabalho/unroll_multiplicacao.py
@@ -3,10 +3,15 @@ from utils import *
 def multiplicacao_matrizes_processos(row_a, col_a, index_row, index_col, processo, results):
     processo = os.fork()
     soma = 0
+    # mutex = multiprocessing.Lock()
+    # mutex.acquire()
     if processo == 0: # se o processo for filho
         for i in range(len(row_a)):
             soma += row_a[i] * col_a[i]
         results[index_row][index_col] = soma
+    else:
+        # o pai espera seus filhos terminarem de processar
+        os.waitpid(processo, 0)
     return processo
 
 def get_col(arr, col):
@@ -22,11 +27,11 @@ def multiplicacao_matrizes_threads(row_a, col_b, index_row, index_col, results):
     results[index_row][index_col] = soma
 
 def unroll(args, func, method, results):
-    # matriz_aleatoria = matriz_randomica(len(args[0]), random.randint(1,3))
-    matriz_aleatoria = [[1,2],[3,4]]
+    matriz_aleatoria = matriz_randomica(len(args[0]), random.randint(1,3))
+    # matriz_aleatoria = [[1,2],[3,4]] (matriz utilizada para testes, saída: [[8,10],[10,16]])
     # ---------- Threads ----------
     # A soma de cada elemento é feito dentro de uma thread
-    if method == "thre":
+    if method == "thread":
         # List das threads criadas
         threads = []
 
@@ -72,19 +77,23 @@ def unroll(args, func, method, results):
             for index, arg in enumerate(args):
                 processos.append([])
                 processo = func(arg, m, index, j, processos[-1], results)
-                processos[-1] = processo
-
+                processos[-1] = processo        
         if len(list(filter(lambda x: x != 0, processos))) == 0: # verifica se todos os processos são filhos
+            # mutex.acquire()
             print("------ Args ------")
             print_matriz(args)
             print("\n------ Aleatoria ------")
             print_matriz(matriz_aleatoria)
             print("\n------ Matriz multiplicada ------")
             print_matriz(results)
+            # processos = []
+            # return
+            # mutex.release()
+
             
             
 
 if __name__ == '__main__':
     res = []
-    unroll([[-1,3],[4,2]], multiplicacao_matrizes_processos, 'proc', res)
-    # unroll([[2,3,1],[-1, 0, 2]], multiplicacao_matrizes_threads, 'thre', res)
+    unroll([[-1,3],[4,2]], multiplicacao_matrizes_processos, 'process', res)
+    # unroll([[2,3,1],[-1, 0, 2]], multiplicacao_matrizes_threads, 'thread', res)
