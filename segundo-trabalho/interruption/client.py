ead9a2ee0eaac1ade749984e7f75695de453c248
Ítalo Epifânio <italoepifaniols@gmail.com>
2019-10-10 17:10:48 -0300

    Cliente criando cobra localmente e enviando para o servidor

--- segundo-trabalho/interruption/client.py
+++ segundo-trabalho/interruption/client.py
@@ -1,12 +1,22 @@
 
-import socket
+import sys
+import time
+import json
+import math
+import random
+import pygame
+import socket, pickle
+import select
+import tkinter as tk
+from tkinter import messagebox
+from snake import snake, cube, randomSnack, redrawWindow
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    cobra = snake((255,0,0), (10,10)) # cria snake do cliente 
+    print(cobra.body)
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
-
-print('Received', repr(data))
+    s.sendall(pickle.dumps(cobra.body)) # enviando cobra do cliente pro servidor
+    # data = s.recv(1024)
--- segundo-trabalho/interruption/server.py
+++ segundo-trabalho/interruption/server.py
@@ -3,14 +3,14 @@ import time
 import math
 import random
 import pygame
-import socket
+import socket, pickle
 import select
 import tkinter as tk
 from tkinter import messagebox
 from snake import snake, cube, randomSnack, redrawWindow
 
 def start_server():
-    port = 65432 
+    port = 65432
 
     read_list = []
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -22,9 +22,13 @@ def start_server():
             readable, writeable, error = select.select(read_list,[],[])
             for sock in readable:
                 if sock is s:
-                    conn, info = sock.accept()
+                    conn, addr = sock.accept()
+                    print("connection received from ", addr)
                     read_list.append(conn)
-                    print("connection received from ", info)
+                    data = conn.recv(4096)
+                    snake = pickle.loads(data) #recebe snake do socket
+                    if(snake):
+                        main(snake)
                 else:
                     data = sock.recv(1024)
                     if data:
@@ -34,44 +38,44 @@ def start_server():
                         sock.close()
                         read_list.remove(sock)
 
-def main():
-    global width, rows, s, snack
-    start_server()
-    # width = 500
-    # rows = 20
-    # win = pygame.display.set_mode((width, width))
-    # s = snake((255,0,0), (10,10))
-    
-    # snack = []
-    # snack.append(cube(randomSnack(rows, s), color=(0,255,0)))
-    # flag = True
-    # clock = pygame.time.Clock()
-    # t = time.clock()
-    # while flag:
-    #     pygame.time.delay(50)
-    #     clock.tick(10)
-    #     s.move()
-    #     if (time.clock() - t) > 0.5: # recupera o tempo
-    #         snack.append(cube(randomSnack(rows, s), color=(0,255,0)))
-    #         t = time.clock() # reseta clock
+def main(snk):
+    # global width, rows, s, snack
+    s = snake((255,0,0), (10,10))
+    s.body = snk
+    # start_server()
+    width = 500
+    rows = 20
+    win = pygame.display.set_mode((width, width))
+    snack = []
+    snack.append(cube(randomSnack(rows, s), color=(0,255,0)))
+    flag = True
+    clock = pygame.time.Clock()
+    t = time.clock()
+    while flag:
+        pygame.time.delay(50)
+        clock.tick(10)
+        s.move()
+        if (time.clock() - t) > 0.5: # recupera o tempo
+            snack.append(cube(randomSnack(rows, s), color=(0,255,0)))
+            t = time.clock() # reseta clock
         
-    #     for i in snack:
-    #         if s.body[0].pos == i.pos:
-    #             s.addCube()
-    #             snack.remove(i)
-    #             snack.append(cube(randomSnack(rows, s), color=(0,255,0)))
-    #             # break
+        for i in snack:
+            if s.body != [] and s.body[0].pos == i.pos:
+                s.addCube()
+                snack.remove(i)
+                snack.append(cube(randomSnack(rows, s), color=(0,255,0)))
+                # break
 
-    #     for x in range(len(s.body)):
-    #         if s.body[x].pos in list(map(lambda z:z.pos,s.body[x+1:])):
-    #             # print('Score: ', len(s.body))
-    #             # message_box('You Lost!', 'Play again...')
-    #             for i in s.body:
-    #                 snack.append(cube(i.pos, color=(0,255,0)))
-    #                 # s.body.remove(i)
-    #             s.reset((10,10))
-    #             break
-    #     redrawWindow(win, rows, width, s, snack)
-    # pass
+        for x in range(len(s.body)):
+            if s.body[x].pos in list(map(lambda z:z.pos,s.body[x+1:])):
+                # print('Score: ', len(s.body))
+                # message_box('You Lost!', 'Play again...')
+                for i in s.body:
+                    snack.append(cube(i.pos, color=(0,255,0)))
+                    # s.body.remove(i)
+                s.reset((10,10))
+                break
+        redrawWindow(win, rows, width, s, snack)
+    pass
 
-main()
+start_server()
