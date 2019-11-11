fb448458541465e1fe3b1dcc31807c995d18dec7
Ítalo Epifânio <italoepifaniols@gmail.com>
2019-11-11 17:41:37 -0300

    Atualiza gitignore. Limpa codigo das threads

--- terceiro-trabalho/thread/src/main.cpp
+++ terceiro-trabalho/thread/src/main.cpp
@@ -7,20 +7,18 @@ int n = 1000;
 
 bool shared_n(int num)
 {
-    // std::cout << num << " ";
     // para evitar aqueles erros citados coloquei um mutex
     // pras threads acessarem o valor n um por vez
     std::mutex mu;
     mu.lock();
     n -= num;
     mu.unlock();
-    // std::cout << n << " ";
-    // return n;
 }
 
 bool isPrime(int num)
 {
-    shared_n(num); // Eu esperava que a função me trouxesse o número atualizado, mas não traz
+    shared_n(num);
+
     int resultado = 0;
     for (int i = 2; i <= n / 2; i++)
     {
@@ -39,6 +37,7 @@ bool isPrime(int num)
     {
         std::cout << n << ": Não!" << std::endl;
     }
+    
     return resultado == 0;
 }
 
@@ -53,13 +52,12 @@ int main()
 
     for (int i = 0; i < n; ++i) // percorre os n numeros
     {
-        // int t_aux = t;
         for (int j = 0; j < t; ++j) // percorre as threads
         {
             threads[j] = std::thread(isPrime, pow(t, (j+1))); // atribui threads ao vetor e manda número n pra função isPrime
         }
         
-        // Sem esperar para matar as threads dá segmentation
+        // Sem esperar as threads dá segmentation
         for (int j = 0; j < t; ++j)
         {
             threads[j].join();
