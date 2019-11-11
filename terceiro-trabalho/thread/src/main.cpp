1ccd82b3c35aebfc93bda3dab53e7ae3cad53e68
Ítalo Epifânio <italoepifaniols@gmail.com>
2019-11-11 18:07:55 -0300

    Dale

--- terceiro-trabalho/thread/src/main.cpp
+++ terceiro-trabalho/thread/src/main.cpp
@@ -5,7 +5,7 @@
 
 int n = 1000;
 
-bool shared_n(int num)
+void shared_n(int num)
 {
     // para evitar aqueles erros citados coloquei um mutex
     // pras threads acessarem o valor n um por vez
@@ -15,48 +15,61 @@ bool shared_n(int num)
     mu.unlock();
 }
 
-bool isPrime(int num)
+void isPrime(int k)
 {
-    shared_n(num);
+    // shared_n(num);
 
     int resultado = 0;
-    for (int i = 2; i <= n / 2; i++)
+    for (int i = 2; i <= k / 2; i++)
     {
-        if (n % i == 0)
+        if (k % i == 0)
         {
             resultado++;
         }
     }
-    
+
     // esses valores ainda estão meio cagados, tem número negativo e tals
-    if (resultado == 0)
+    if (k < 0)
     {
-        std::cout << n << ": Primo!" << std::endl;
+
+        if (resultado == 0)
+        {
+            std::cout << k << ": Primo!" << std::endl;
+        }
+        else
+        {
+            std::cout << k << ": Não!" << std::endl;
+        }
     }
-    else
-    {
-        std::cout << n << ": Não!" << std::endl;
-    }
-    
-    return resultado == 0;
 }
 
 int main()
 {
     static const int t = 2;
     std::thread threads[t];
+    int ns[t]; // cria n da threads
 
-    if(n % 2 == 0){
+    // Se o número n for par ele se torna ímpar
+    if (n % 2 == 0)
+    {
         n -= 1;
     }
 
-    for (int i = 0; i < n; ++i) // percorre os n numeros
+    for (int j = 0; j < t; ++j)
+    {
+        ns[j] = n;
+    }
+
+    int k = n;
+
+    for (int i = 0; i < k; ++i) // percorre os n numeros
     {
         for (int j = 0; j < t; ++j) // percorre as threads
         {
-            threads[j] = std::thread(isPrime, pow(t, (j+1))); // atribui threads ao vetor e manda número n pra função isPrime
+            threads[j] = std::thread(isPrime, (ns[j] - (pow(t, (j + 1))))); // atribui threads ao vetor e manda número n pra função isPrime
+            ns[j] -= pow(t, (j + 1));
         }
-        
+
         // Sem esperar as threads dá segmentation
         for (int j = 0; j < t; ++j)
         {
