73e5a30fb790c06d4dd2919e578108ef1665e047
Epitacio <bessa.epitacio@gmail.com>
2019-11-11 17:41:43 -0300

    Melhorando algoritmo e fazendo pequenos ajustes

--- terceiro-trabalho/sequencial/src/main.cpp
+++ terceiro-trabalho/sequencial/src/main.cpp
@@ -1,26 +1,28 @@
 #include <iostream>
+#include <math.h>
 
 bool isPrime(int n) {
-    int resultado = 0;
-    for (int i = 2; i <= n/2; i++) {
+    bool resultado = true;
+    for (int i = 2; i <= sqrt(n); i++) {
         if (n % i == 0) {
-            resultado++;
+            resultado = false;
+            break;
         }
     }
     
-    return resultado == 0;
+    return resultado;
 }
 
 int main() {
     int n = 100000;
+    int cont = 0;
 
     for (int i = 1; i <= n; i++) {
         if (isPrime(i)) {
-            std::cout << i << ": Primo!" << std::endl;
-        } else {
-            std::cout << i << ": Não!" << std::endl;
+            cont++;
         }
     }
+    std::cout << cont << std::endl;
 
     return 0;
 }
